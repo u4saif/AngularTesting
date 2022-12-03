d65bbf9adc8033c2bfda51ce3be67becba6f0913
eysaif <shaikh.saifuddin@in.ey.com>
2022-12-03 22:00:56 +0530

    code prettier

--- src/services/app.service.spec.ts
+++ src/services/app.service.spec.ts
@@ -1,15 +1,18 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { AppService } from './app.service';
 
 describe('AppService', () => {
   let service: AppService;
-  let HttpController:HttpTestingController;
+  let HttpController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(AppService);
     HttpController = TestBed.inject(HttpTestingController);
@@ -19,21 +22,23 @@ describe('AppService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should add two numbers',()=>{
-    expect(service.addNumber(4,3)).toEqual(7);
+  it('should add two numbers', () => {
+    expect(service.addNumber(4, 3)).toEqual(7);
   });
 
-  it('should be called Http Tests',()=>{
-    service.getData().subscribe((response)=>{
+  it('should be called Http Tests', () => {
+    service.getData().subscribe((response) => {
       expect(response).toBeTruthy('No data returned');
-      expect(response).toEqual({ payload: { name: 'saif' }});
+      expect(response).toEqual({ payload: { name: 'saif' } });
     });
 
-    const req=HttpController.expectOne('https://ap.unsplash.com/photos?page=1&query=random&client_id=Wodf-s3S_rzzMqYGrFLhqunWZMOEDAqvSqX3Gci6DVM');
-    
-    expect(req.request.method).toEqual('GET'); 
+    const req = HttpController.expectOne(
+      'https://ap.unsplash.com/photos?page=1&query=random&client_id=Wodf-s3S_rzzMqYGrFLhqunWZMOEDAqvSqX3Gci6DVM'
+    );
+
+    expect(req.request.method).toEqual('GET');
     req.flush({
-      payload:{name:"saif"}
+      payload: { name: 'saif' },
     });
   });
 });
