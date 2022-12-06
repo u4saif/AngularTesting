f21ae06b703ab44dbb7327cce3cc86c95c0c69bd
eysaif <shaikh.saifuddin@in.ey.com>
2022-12-06 12:41:03 +0530

    first cypress End-to-End test

--- cypress/integration/appService.spec.ts
+++ cypress/integration/appService.spec.ts
@@ -3,6 +3,10 @@ describe("First Cypress Test Running",()=>{
     it("Should Show first Running passed",()=>{
         expect(true).to.be.true;
         expect(42).to.equal(42);
-        cy.visit('/');
+    });
+
+    it("Should Show End to End Test",()=>{
+       cy.visit('/');
+       cy.contains("Welcome to Angular Testing");
     });
 });
