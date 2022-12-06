8ed5642a7dafa466e2453ebeef3a20c297c8c8dc
eysaif <shaikh.saifuddin@in.ey.com>
2022-12-06 14:44:40 +0530

    cypress end2end test completed

--- cypress/integration/appService.spec.ts
+++ cypress/integration/appService.spec.ts
@@ -9,4 +9,14 @@ describe("First Cypress Test Running",()=>{
        cy.visit('/');
        cy.contains("Welcome to Angular Testing");
     });
+
+    it("HomePage End to End Test with MockBackend API",()=>{
+        cy.fixture('users.json').as('usersJson');
+        cy.server();
+        cy.route("https://api.unsplash.com/photos?page=1&query=random&client_id=Wodf-s3S_rzzMqYGrFLhqunWZMOEDAqvSqX3Gci6DVM","@usersJson").as('cardData');
+        cy.visit('/');
+        cy.contains("Welcome to Angular Testing");
+        cy.wait("@cardData");
+        cy.get("div").should("have.length",11);
+     });
 });
